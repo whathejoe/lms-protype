2520adee848ab6125e4788cf5aeeec01a015fefd
whathejoe <clvillarealiv@gmail.com>
2016-01-25 10:23:25 +0800

    Created lesson-4.js

---
+++ pages/quizzes/app_data/lesson-4.js
@@ -0,0 +1,101 @@
+var app = angular.module('quizApp', []);
+
+app.directive('quiz', function(quizFactory) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		templateUrl: 'mc-template.html',
+		link: function(scope, elem, attrs) {
+			
+			scope.lesson = "Lesson 3";
+
+			scope.start = function() {
+				scope.id = 0;
+				scope.items = 0;
+				scope.quizOver = false;
+				scope.inProgress = true;
+				scope.getQuestion();
+			};
+
+			scope.reset = function() {
+				scope.inProgress = false;
+				scope.score = 0;
+			}
+
+			scope.getQuestion = function() {
+				var q = quizFactory.getQuestion(scope.id);
+				if(q) {
+					scope.question = q.question;
+					scope.options = q.options;
+					scope.answer = q.answer;
+					scope.answerMode = true;
+					scope.items++;
+				} else {
+					scope.quizOver = true;
+				}
+			};
+
+			scope.checkAnswer = function() {
+				if(!$('input[name=answer]:checked').length) return;
+
+				var ans = $('input[name=answer]:checked').val();
+
+				if(ans == scope.options[scope.answer]) {
+					scope.score++;
+					scope.correctAns = true;
+				} else {
+					scope.correctAns = false;
+				}
+
+				scope.answerMode = false;
+			};
+
+			scope.nextQuestion = function() {
+				scope.id++;
+				scope.getQuestion();
+			}
+
+			scope.reset();
+		}
+	}
+});
+
+app.factory('quizFactory', function() {
+	var questions = [
+		{
+			question: "Which number represents - Fifty-five thousand sixteen?",
+			options: ["55 016", "54 016", "55 060", "55 160"],
+			answer: 0
+		},
+		{
+			question: "Which number represents - Sixty-seven thousand two hundred three?",
+			options: ["67 230", "67 203", "60 230", "67 200"],
+			answer: 1
+		},
+		{
+			question: "Which number represents - Eighty-four thousand nine?",
+			options: ["84 000", "80 000", "84 009", "90 009"],
+			answer: 2
+		},
+		{
+			question: "Which number represents - Fifty-seven thousand two?",
+			options: ["57 000", "7 000", "50 000", "57 002"],
+			answer: 3
+		},
+		{
+			question: "Which number represents - One hundred thousand?",
+			options: ["1 000", "100 000", "10 000", "10"],
+			answer: 1
+		}
+	];
+
+	return {
+		getQuestion: function(id) {
+			if(id < questions.length) {
+				return questions[id];
+			} else {
+				return false;
+			}
+		}
+	};
+});
