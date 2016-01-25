01f6d19fe9adba454d079546051435b4709adb6e
whathejoe <clvillarealiv@gmail.com>
2016-01-25 10:31:52 +0800

    Created lesson-5.js

---
+++ pages/quizzes/app_data/lesson-5.js
@@ -0,0 +1,101 @@
+var app = angular.module('quizApp', []);
+
+app.directive('quiz', function(quizFactory) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		templateUrl: 'mc-template.html',
+		link: function(scope, elem, attrs) {
+			
+			scope.lesson = "Lesson 4";
+
+			scope.start = function() {
+				scope.id = 0;
+				scope.items = 0;
+				scope.quizOver = false;
+				scope.inProgress = true;
+				scope.getQuestion();
+			};
+
+			scope.reset = function() {
+				scope.inProgress = false;
+				scope.score = 0;
+			}
+
+			scope.getQuestion = function() {
+				var q = quizFactory.getQuestion(scope.id);
+				if(q) {
+					scope.question = q.question;
+					scope.options = q.options;
+					scope.answer = q.answer;
+					scope.answerMode = true;
+					scope.items++;
+				} else {
+					scope.quizOver = true;
+				}
+			};
+
+			scope.checkAnswer = function() {
+				if(!$('input[name=answer]:checked').length) return;
+
+				var ans = $('input[name=answer]:checked').val();
+
+				if(ans == scope.options[scope.answer]) {
+					scope.score++;
+					scope.correctAns = true;
+				} else {
+					scope.correctAns = false;
+				}
+
+				scope.answerMode = false;
+			};
+
+			scope.nextQuestion = function() {
+				scope.id++;
+				scope.getQuestion();
+			}
+
+			scope.reset();
+		}
+	}
+});
+
+app.factory('quizFactory', function() {
+	var questions = [
+		{
+			question : "Round off 56 456 to the nearest ten thousands.",
+			options: ["60 000", "70 000", "56 400", "56 000"],
+			answer: 0
+		},
+		{
+			question: "Round off 43 840 to the nearest hundreds.",
+			options: ["43 800", "43 000", "44 000", "43 900"],
+			answer: 0
+		},
+		{
+			question: "Round off 14 500 to the nearest thousands.",
+			options: ["13 500", "13 000", "14 000", "15 000"],
+			answer: 3
+		},
+		{
+			question: "Round off 98 600 to the nearest thousands.",
+			options: ["90 000", "99 000", "98 500", "90 600"],
+			answer: 1
+		},
+		{
+			question: "Round off 56 986 to the nearest ten thousands.",
+			options: ["50 000", "60 000", "70 000", "80 000"],
+			answer: 1
+		}
+	];
+
+	return {
+		getQuestion: function(id) {
+			if(id < questions.length) {
+				return questions[id];
+			} else {
+				return false;
+			}
+		}
+	};
+});
