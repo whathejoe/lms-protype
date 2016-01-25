a28b4ae46b3424406eaa2f84f73d4c523473556f
whathejoe <clvillarealiv@gmail.com>
2016-01-25 22:28:00 +0800

    Added lesson 22 quiz

---
+++ pages/quizzes/app_data/lesson-22.js
@@ -0,0 +1,96 @@
+var app = angular.module('quizApp', []);
+
+app.directive('quiz', function(quizFactory) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		templateUrl: 'mc-template.html',
+		link: function(scope, elem, attrs) {
+			
+			scope.lesson = "Lesson 22";
+
+			scope.start = function() {
+				scope.id = 0;
+				scope.items = 0;
+				scope.quizOver = false;
+				scope.inProgress = true;
+				scope.getQuestion();
+			};
+
+			scope.reset = function() {
+				scope.inProgress = false;
+				scope.score = 0;
+			}
+
+			scope.getQuestion = function() {
+				var q = quizFactory.getQuestion(scope.id);
+				if(q) {
+					scope.question = q.question;
+					scope.options = q.options;
+					scope.answer = q.answer;
+					scope.answerMode = true;
+					scope.items++;
+				} else {
+					scope.quizOver = true;
+				}
+			};
+
+			scope.checkAnswer = function() {
+				if(!$('input[name=answer]:checked').length) return;
+
+				var ans = $('input[name=answer]:checked').val();
+
+				if(ans == scope.options[scope.answer]) {
+					scope.score++;
+					scope.correctAns = true;
+				} else {
+					scope.correctAns = false;
+				}
+
+				scope.answerMode = false;
+			};
+
+			scope.nextQuestion = function() {
+				scope.id++;
+				scope.getQuestion();
+			}
+
+			scope.reset();
+		}
+	}
+});
+
+app.factory('quizFactory', function() {
+	var questions = [
+		{
+			question : "What is the remainder when 6567 is divided by 27?",
+			options: ["264 r. 13", "243 r. 6", "251", "263"],
+			answer: 0
+		},
+		{
+			question : "A farmer planted 1680 pineapples equally in 24 rows. How many pineapples were planted in each row?",
+			options: ["180", "240", "160", "210"],
+			answer: 2
+		},
+		{
+			question : "Hazel, Cha, and Marimar shared the amount of Php480 to buy a birthday gift for their teacher. How much did each of them share?",
+			options: ["120", "160", "180", "140"],
+			answer: 1
+		},
+		{
+			question : "A farmer gathered 760 mangoes from his farm. He placed 60 mangoes in each basket and sold them. How many mangoes were not in a full basket?",
+			options: ["20", "30", "40", "50"],
+			answer: 3
+		}
+	];
+
+	return {
+		getQuestion: function(id) {
+			if(id < questions.length) {
+				return questions[id];
+			} else {
+				return false;
+			}
+		}
+	};
+});
